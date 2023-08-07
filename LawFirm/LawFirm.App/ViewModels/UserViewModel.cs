572654992d86fef65083fb1e4be0ea2b181f79ee
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2023-08-06 21:29:39 -0500

    Initializing loding bar added and create user partially implemeted

---
+++ LawFirm/LawFirm.App/ViewModels/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace LawFirm.App.ViewModels;
+
+public class UserViewModel
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+
+public class UserViewModelValidation : AbstractValidator<UserViewModel>
+{
+    public UserViewModelValidation()
+    {
+        RuleFor(x => x.FirstName).NotEmpty();
+        RuleFor(x => x.LastName).NotEmpty();
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.UserName).NotEmpty();
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
