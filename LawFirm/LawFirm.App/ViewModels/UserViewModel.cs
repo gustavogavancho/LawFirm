4e6182b237302bcf15762917083224c16ba45bb3
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2023-08-19 18:29:10 -0500

    Block special characters while registering a new user

--- LawFirm/LawFirm.App/ViewModels/UserViewModel.cs
+++ LawFirm/LawFirm.App/ViewModels/UserViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace LawFirm.App.ViewModels;
 
@@ -19,8 +20,16 @@ public class UserViewModelValidation : AbstractValidator<UserViewModel>
         RuleFor(x => x.FirstName).NotEmpty();
         RuleFor(x => x.LastName).NotEmpty();
         RuleFor(x => x.Email).NotEmpty().EmailAddress();
-        RuleFor(x => x.UserName).NotEmpty();
+        RuleFor(x => x.UserName).NotEmpty().Must(BeFreeFromSpecialCharacters).WithMessage("Username cannot contain special characters.");
         RuleFor(x => x.Password).NotEmpty();
         RuleFor(x => x.ConfirmPassword).Equal(x => x.Password).WithMessage("Password and Confirm Password must match");
+    }
+
+    private bool BeFreeFromSpecialCharacters(string username)
+    {
+        // Define your own logic to check for special characters.
+        // You can use regular expressions or any other method you prefer.
+        // Here's a simple example using regular expression to disallow special characters.
+        return !Regex.IsMatch(username, @"[@!#$%^&*()_+{}\[\]:;<>,.?~\\/-]") && !username.Contains("ñ", StringComparison.OrdinalIgnoreCase);
     }
 }
