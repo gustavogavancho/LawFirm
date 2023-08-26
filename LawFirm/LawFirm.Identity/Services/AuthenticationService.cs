e45718dfa3f65216193c3be3567496cc1935d0de
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2023-08-26 16:54:53 -0500

    Roles issue fixed

--- LawFirm/LawFirm.Identity/Services/AuthenticationService.cs
+++ LawFirm/LawFirm.Identity/Services/AuthenticationService.cs
@@ -126,18 +126,18 @@ public class AuthenticationService : IAuthenticationService
 
         var roleClaims = new List<Claim>();
 
-        for (int i = 0; i < roles.Count; i++)
+        foreach (var role in roles)
         {
-            roleClaims.Add(new Claim("roles", roles[i]));
+            roleClaims.Add(new Claim(ClaimTypes.Role, role)); // Use ClaimTypes.Role for role claims
         }
 
         var claims = new[]
         {
-                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                new Claim("uid", user.Id)
-        }
+        new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        new Claim(JwtRegisteredClaimNames.Email, user.Email),
+        new Claim("uid", user.Id)
+    }
         .Union(userClaims)
         .Union(roleClaims);
 
@@ -150,6 +150,7 @@ public class AuthenticationService : IAuthenticationService
             claims: claims,
             expires: DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMinutes),
             signingCredentials: signingCredentials);
+
         return jwtSecurityToken;
     }
 
