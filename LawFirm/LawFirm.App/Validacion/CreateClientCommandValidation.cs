b9808f6316715df4f63a163e7e7edba38f654bfe
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2023-10-05 20:37:05 -0500

    Improve client validation

--- LawFirm/LawFirm.App/Validacion/CreateClientCommandValidation.cs
+++ LawFirm/LawFirm.App/Validacion/CreateClientCommandValidation.cs
@@ -8,9 +8,21 @@ public class CreateClientCommandValidation : AbstractValidator<CreateClientComma
     public CreateClientCommandValidation()
     {
         RuleFor(x => x.ClientType).NotEmpty();
-        RuleFor(x => x.Nit).NotNull();
+        RuleFor(x => x.Nit).NotNull().Must(nit => nit.ToString().Length == 8 || nit.ToString().Length == 11).WithMessage("Nit must be either 8 or 11 digits.");
         RuleFor(x => x.PhoneNumber).NotEmpty();
         RuleFor(x => x.Address).NotEmpty();
         RuleFor(x => x.Email).NotEmpty();
+
+        When(x => x.ClientType == "Natural Person", () =>
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+        });
+
+        When(x => x.ClientType == "Legal Person", () =>
+        {
+            RuleFor(x => x.BusinessName).NotNull();
+            RuleFor(x => x.Representative).NotNull();
+        });
     }
 }
