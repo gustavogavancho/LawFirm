f284fc50909bc26bd92ad4ee9d47736f4d6ef29f
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2024-07-23 23:08:54 -0500

    Consulting fee form added

---
+++ LawFirm/LawFirm.App/Validacion/AddConsultingFeeVmValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using LawFirm.App.Services.Base;
+
+namespace LawFirm.App.Validacion;
+
+public class AddConsultingFeeVmValidation : AbstractValidator<ConsultingFeeVM>
+{
+    public AddConsultingFeeVmValidation()
+    {
+        RuleFor(x => x.TotalAmmount).NotNull();
+        RuleFor(x => x.ExpirationDate).NotNull();
+    }
+}
